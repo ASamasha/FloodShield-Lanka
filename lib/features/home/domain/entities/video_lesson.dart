aabaae5e58672786dd7b24d1cf1e65ddaa40cf76
Amasha Sithmini <sithminiamasha@gmail.com>
2026-09-10 14:16:42 +0530

    video_lesson

---
+++ lib/features/home/domain/entities/video_lesson.dart
@@ -0,0 +1,11 @@
+class VideoLesson {
+  final String title;
+  final String duration;
+  final double progressPercent;
+
+  VideoLesson({
+    required this.title,
+    required this.duration,
+    required this.progressPercent,
+  });
+}
