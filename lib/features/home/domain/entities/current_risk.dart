37dc0937b82d698692997f65663aad3497738e0b
Amasha Sithmini <sithminiamasha@gmail.com>
2026-09-10 14:14:53 +0530

    Add CurrentRisk entity model for home dashboard status

---
+++ lib/features/home/domain/entities/current_risk.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CurrentRisk {
+  final String level;
+  final String instructions;
+  final Color color;
+
+  CurrentRisk({
+    required this.level,
+    required this.instructions,
+    required this.color,
+  });
+}
